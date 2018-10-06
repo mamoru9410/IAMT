1ea6fde6fa4e742b7796ab5655093e6500be0f88
taku11 <no9.noid6@gmail.com>
2018-10-06 18:06:24 +0900

    add - react

---
+++ react/zepetto/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export default props => (
+  <HomeComponent>
+    dividvijdivjajseflarlnflaer dfa;osrhfghanlserhfila'enfa
+    vasdvoia;dvhaoi;shofhadsv adgva;osfhaos'hfa;ohes'fvadsv
+    adsanernfaorf'ajsdlvas fasdfaekfaojedfoasd'fvand'vpar pofv
+  </HomeComponent>
+);
+const HomeComponent = styled.HomeComponent``;
